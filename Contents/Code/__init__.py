7fee1f4ac134d3d68d24c80455d73e4a0d9633d8
Mike <mike.d.meier@gmail.com>
2014-04-19 11:44:35 -0600

    check value of "hidden" key rather than presence of it

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -359,7 +359,7 @@ def genCode(length=20):
 def CheckForUpdates(install=False, return_message=False, plugin=None):
     #use the github commit feed for each installed plugin to check for available updates
     if plugin:
-        if plugin['hidden']:
+        if plugin['hidden'] == "true":
             return ObjectContainer(header="Unsupported Appstore", message="%s : No longer supported. No updates." % plugin['title'])
         GetRSSFeed(plugin=plugin, install=install)
         if return_message:
